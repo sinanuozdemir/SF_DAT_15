8630b9699c00e3c8b4b2c323804ecfa192863015
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2015-06-24 18:03:16 -0700

    removed file for class 4

--- code/04_pandas_2_lab.py
+++ code/04_pandas_2_lab.py
@@ -23,7 +23,7 @@ ufo = pd.read_csv('https://raw.githubusercontent.com/sinanuozdemir/SF_DAT_15/mas
 
 # 6. Which shape is reported most often?
 
-# 7. Plot number of sights per day in 2014 (days should be in order!)
+# 7. Plot number of sightings per day in 2014 (days should be in order!)
 
 
 
