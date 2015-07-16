cf7d569758c4dad1d1a39ce80f2f78bce526e22c
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2015-07-15 17:25:11 -0700

    Added class 10 material

---
+++ hw/hw2.py
@@ -0,0 +1,70 @@
+##### Part 1 #####
+
+
+# 1. read in the yelp dataset
+
+# 2. Perform a linear regression using 
+# "stars" as your response and 
+# "cool", "useful", and "funny" as predictors
+
+# 3. Show your MAE, R_Squared and RMSE
+
+# 4. Use statsmodels to show your pvalues
+# for each of the three predictors
+# Using a .05 confidence level, 
+# Should we eliminate any of the three?
+
+# 5. Create a new column called "good_rating"
+# this could column should be True iff stars is 4 or 5
+# and False iff stars is below 4
+
+# 6. Perform a Logistic Regression using 
+# "good_rating" as your response and the same
+# three predictors
+
+
+# 7. Show your Accuracy, Sensitivity, Specificity
+# and Confusion Matrix
+
+
+# 8. Perform one NEW operation of your 
+# choosing to try to boost your metrics!
+
+
+
+
+##### Part 2 ######
+
+# 1. Read in the titanic data set.
+
+# 4. Create a new column called "wife" that is True
+# if the name of the person contains Mrs.
+# AND their SibSp is at least 1
+
+# 5. What is the average age of a male and
+# the average age of a female on board?
+
+# 5. Fill in missing MALE age values with the
+# average age of the remaining MALE ages
+
+# 6. Fill in missing FEMALE age values with the
+# average age of the remaining FEMALE ages
+
+
+# 7. Perform a Logistic Regression using
+# Survived as your response and age, wife
+# as predictors
+
+# 8. Show Accuracy, Sensitivity, Specificity and 
+# Confusion matrix
+
+
+# 9. now use ANY of your variables as predictors
+# Still using survived as a response to boost metrics!
+
+
+# 10. Show Accuracy, Sensitivity, Specificity
+
+
+
+
